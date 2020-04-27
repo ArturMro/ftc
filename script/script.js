ccde1bd5f4abc282c5972fee4382fc9a7da70560
ArturMro <58179023+ArturMro@users.noreply.github.com>
2020-04-27 20:57:44 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,26 @@
+console.log('Script file loaded.');
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  console.log('HTML file loaded and parsed.');
+
+  addMyEventListeners();
+  
+  
+    
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
